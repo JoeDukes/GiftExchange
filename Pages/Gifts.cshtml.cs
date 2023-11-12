4053090b7711627a4d5dad39ddf621e13cbbd0e9
Joe Dukes (S1oop) <joedukes@gmail.com>
2023-11-11 19:31:53 -0500

    first commit

---
+++ Pages/Gifts.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GiftExchange.Pages;
+
+public class GiftsModel : PageModel
+{
+    private readonly ILogger<GiftsModel> _logger;
+
+    public GiftsModel(ILogger<GiftsModel> logger)
+    {
+        _logger = logger;
+    }
+
+    [BindProperty]
+    public string Recipient {get; set;} = "";
+
+    public IActionResult OnGet([FromRoute] string? recipient)
+    {
+        if(String.IsNullOrEmpty(recipient))
+        {
+            return RedirectToPage("./Index");
+        }
+
+        Recipient = recipient;
+
+        return Page();
+    }
+}
+
