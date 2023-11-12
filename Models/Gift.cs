2b545914ef7935ddb96ef244863ea700a868afb0
Joe Dukes (S1oop) <joedukes@gmail.com>
2023-11-11 22:05:59 -0500

    Start EF model

--- Models/Gift.cs
+++ Models/Gift.cs
@@ -1,10 +1,40 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace GiftExchange.Models;
 
+internal class GiftContext : DbContext
+{
+    public DbSet<Gift> Gifts {get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            @""
+        );
+    }
+}
+
+[Table("Gifts")]
 public class Gift
 {
     public int Id {get; set;}
     public int UserId {get; set;}
     public string gift {get; set;} = "UnderPanties";
+}
+
+enum family
+{
+    Russ,
+    Sue,
+    Jeff,
+    Heather,
+    Harry,
+    Paxson,
+    Joe,
+    Dana,
+    Oliver,
+    Megan,
+    Mike,
 }
